6b35885d39220c6c8f9030309896e5f3daf18cfc
leeflyiny <18550169074@163.com>
2020-06-21 07:56:55 +0800

    更新线程交互打印demo

---
+++ src/main/java/com/lifei/study/threadstudy/TwoThreadPrint.java
@@ -0,0 +1,52 @@
+package com.lifei.study.threadstudy;
+
+public class TwoThreadPrint implements Runnable
+{
+
+    private int i = 0;
+
+    private String lock = "hello";
+
+    public static void main(String[] args)
+    {
+        TwoThreadPrint twoThreadPrint = new TwoThreadPrint();
+        Thread t1 = new Thread(twoThreadPrint);
+        Thread t2 = new Thread(twoThreadPrint);
+        Thread t3 = new Thread(twoThreadPrint);
+
+        t1.start();
+        t2.start();
+        t3.start();
+    }
+
+    @Override
+    public void run()
+    {
+        while (true)
+        {
+            synchronized (lock)
+            {
+                i++ ;
+                System.out.println(Thread.currentThread().getName() + ": " + i);
+                try
+                {
+                    Thread.sleep(500);
+                }
+                catch (InterruptedException e)
+                {
+                    System.out.println(e);
+                }
+//                lock.notifyAll();
+
+                try
+                {
+                    lock.wait();
+                }
+                catch (InterruptedException e)
+                {
+                    System.out.println(e);
+                }
+            }
+        }
+    }
+}
