d1b51cdd57d3c16ef784085edccd840d31b5eec9
lifei <18550169074@163.com>
2020-09-18 08:32:29 +0800

    更新配置文件

--- src/main/java/com/lifei/algorithm/LeetCode39.java
+++ src/main/java/com/lifei/algorithm/LeetCode39.java
@@ -1,8 +1,8 @@
 package com.lifei.algorithm;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
+import java.util.Deque;
 import java.util.List;
 
 /**
@@ -16,34 +16,78 @@ import java.util.List;
  * @Date: 2020/9/11
  */
 public class LeetCode39 {
-    public static List<List<Integer>> combinationSum(int[] candidates, int target) {
-        Arrays.sort(candidates);
-        List<List<Integer>> res = new ArrayList<List<Integer>>();
-        List<Integer> list = new ArrayList<Integer>();
-        return getRightNums(res, target, 0, list, candidates);
+
+    public static class LeetCodePersonal39 {
+
+        public static List<List<Integer>> combinationSum(int[] candidates, int target) {
+            int len = candidates.length;
+            List<List<Integer>> res = new ArrayList<>();
+            if (len == 0) {
+                return res;
+            }
+            Deque<Integer> deque = new ArrayDeque<>();
+            dfs(candidates, target, res, 0, deque);
+            return res;
+        }
+
+        private static void dfs(int[] candidates, int target, List<List<Integer>> res, int beginIndex, Deque<Integer> deque) {
+            if (target < 0)
+                return;
+            if (target == 0) {
+                res.add(new ArrayList<>(deque));
+                return;
+            }
+            for (int i = beginIndex; i < candidates.length; i++) {
+                deque.addLast(candidates[i]);
+                dfs(candidates, target - candidates[i], res, i, deque);
+                deque.removeLast();
+            }
+        }
+
     }
 
-    private static List<List<Integer>> getRightNums(List<List<Integer>> list, int target, int index, List<Integer> temp, int[] candidates) {
+    public static List<List<Integer>> combinationSum(int[] candidates, int target) {
+//        Arrays.sort(candidates);
+        int len = candidates.length;
+        List<List<Integer>> res = new ArrayList<>();
+        if (len == 0) {
+            return res;
+        }
+        // 双端队列
+        Deque<Integer> path = new ArrayDeque<>();
+        dfs(candidates, 0, len, target, path, res);
+        return res;
+    }
+
+    /**
+     * @param candidates 候选数组
+     * @param begin      搜索起点
+     * @param len        冗余变量，是 candidates 里的属性，可以不传
+     * @param target     每减去一个元素，目标值变小
+     * @param path       从根结点到叶子结点的路径，是一个栈
+     * @param res        结果集列表
+     */
+    private static void dfs(int[] candidates, int begin, int len, int target, Deque<Integer> path, List<List<Integer>> res) {
+        // target 为负数和 0 的时候不再产生新的孩子结点
+        if (target < 0) {
+            return;
+        }
         if (target == 0) {
-            list.add(temp);
-            return list;
+            res.add(new ArrayList<>(path));
+            return;
         }
-        if (index >= target) {
-            list.remove(list.size() - 1);
-            return list;
+
+        // 重点理解这里从 begin 开始搜索的语意
+        for (int i = begin; i < len; i++) {
+            // 预制当前候选数据
+            path.addLast(candidates[i]);
+
+            // 注意：由于每一个元素可以重复使用，下一轮搜索的起点依然是 i，这里非常容易弄错
+            dfs(candidates, i, len, target - candidates[i], path, res);
+
+            // 状态重置，删除预制节点
+            path.removeLast();
         }
-        target = target - candidates[index];
-        if (target == 0) {
-            list.add(temp);
-        } else if (target > 0) {
-            temp.add(candidates[index]);
-            getRightNums(list, target, index, temp, candidates);
-            getRightNums(list, target, index + 1, temp, candidates);
-        } else {
-//            list.remove(list.size() - 1);
-            return list;
-        }
-        return list;
     }
 
     public static void main(String[] args) {
--- src/main/java/com/lifei/algorithm/LeetCode40.java
+++ src/main/java/com/lifei/algorithm/LeetCode40.java
@@ -1,20 +1,59 @@
 package com.lifei.algorithm;
 
-import java.util.List;
+import java.util.*;
 
 /**
  * 40. 组合总和 II
  * 给定一个数组candidates和一个目标数target，
  * 找出candidates中所有可以使数字和为target的组合。
- *
+ * <p>
  * candidates中的每个数字在每个组合中只能使用一次。
-
+ *
  * @Author: lifei
  * @Description:
  * @Date: 2020/9/10
  */
 public class LeetCode40 {
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-        return null;
+        List<List<Integer>> list = new ArrayList<>();
+        int length = candidates.length;
+        if (length == 0)
+            return list;
+
+
+        // 排序 剪枝
+        Arrays.sort(candidates);
+
+        Deque<Integer> deque = new ArrayDeque<>();
+        dfs(candidates, target, list, 0, deque);
+        return list;
+    }
+
+    private static void dfs(int[] candidates, int target, List<List<Integer>> list, int beginIndex, Deque<Integer> deque) {
+        if (target < 0)
+            return;
+
+        if (target == 0) {
+            list.add(new ArrayList<>(deque));
+            return;
+        }
+
+        for (int i = beginIndex; i < candidates.length; i++) {
+            // 大剪枝：减去 candidates[i] 小于 0，减去后面的 candidates[i + 1]、candidates[i + 2] 肯定也小于 0，因此用 break
+            if (target - candidates[i] < 0) {
+                break;
+            }
+            if (i > 0 && candidates[i] == candidates[i - 1])
+                continue;
+            // 小剪枝：同一层相同数值的结点，从第 2 个开始，候选数更少，结果一定发生重复，因此跳过，用 continue
+            if (i > beginIndex && candidates[i] == candidates[i - 1]) {
+                continue;
+            }
+            // 先添加上
+            deque.addLast(candidates[i]);
+            dfs(candidates, target - candidates[i], list, i + 1, deque);
+            // 在本层for循环 重置I的状态
+            deque.removeLast();
+        }
     }
 }
